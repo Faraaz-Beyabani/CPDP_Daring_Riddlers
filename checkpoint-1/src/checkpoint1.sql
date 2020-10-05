93e517b5b1c7eae8b4c8f3c9244903ddf62bfd93
Andrew Vagliano <andrewvagliano2021@u.northwestern.edu>
2020-10-05 18:47:01 -0500

    Added allegation queries

--- checkpoint-1/src/checkpoint1.sql
+++ checkpoint-1/src/checkpoint1.sql
@@ -1,7 +1,47 @@
 -- Which 5 counties have the highest crime rate, number of assigned police officers, complaints, 
 -- and settlements (for police misconduct) per 100,000 residents?
 
+-- Officers by Unit
+
+-- Allegations by Beat
+SELECT
+    beat_id as "Beat ID",
+    COUNT(*) as "Allegations"
+FROM data_allegation
+WHERE
+    beat_id is not NULL AND
+    EXTRACT(year FROM incident_date) = 2000
+GROUP BY beat_id
+ORDER BY COUNT(*) DESC
+
+SELECT
+    beat_id as "Beat ID",
+    COUNT(*) as "Allegations"
+FROM data_allegation
+WHERE
+    beat_id is not NULL AND
+    EXTRACT(year FROM incident_date) = 2015
+GROUP BY beat_id
+ORDER BY COUNT(*) DESC
+
+
 -- What is the average for these 4 statistics across all neighborhoods in Chicago / the Chicago area?
+
+
+
+-- Total Officers
+
+-- Total Allegations
+SELECT COUNT(*)
+FROM data_allegation
+WHERE EXTRACT(year FROM incident_date) = 2005
+
+SELECT COUNT(*)
+FROM data_allegation
+WHERE EXTRACT(year FROM incident_date) = 2015
+
+-- Total Settlements
+
 
 -- What is the racial and gender breakdown of the policing force (aggregating across all neighborhoods)? 
 SELECT race, count(race), gender, count(gender)
