2fea45f632326fcb933e214b365c5d955d7fa87a
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-10-05 14:48:28 -0500

    Fix minor bug with old query, add gender ratio query per unit

--- checkpoint-1/src/checkpoint1.sql
+++ checkpoint-1/src/checkpoint1.sql
@@ -7,16 +7,26 @@
 SELECT race, count(race), gender, count(gender)
 FROM data_officer
 WHERE EXTRACT(year FROM appointed_date) <= 2000
-AND EXTRACT(year FROM resignation_date) = 2000 OR resignation_date is null
+AND EXTRACT(year FROM resignation_date) >= 2000 OR resignation_date is null
 GROUP BY race, gender
 
 SELECT race, count(race), gender, count(gender)
 FROM data_officer
 WHERE EXTRACT(year FROM appointed_date) <= 2015
-AND EXTRACT(year FROM resignation_date) = 2015 OR resignation_date is null
+AND EXTRACT(year FROM resignation_date) >= 2015 OR resignation_date is null
 GROUP BY race, gender
 
--- Which 5 counties have the highest male-to-female ratio of police officers?
+-- Which units have the highest male-to-female ratio of police officers?
+
+SELECT dpu.description, dof.gender, count(dof.gender)
+FROM data_officer dof, data_officerhistory doh, data_policeunit dpu
+WHERE doh.officer_id = dof.id AND doh.unit_id = dpu.id AND dpu.description IS NOT NULL
+GROUP BY dpu.description, dof.gender;
+
+SELECT dpu.description, round(cast((cast(sum(CASE WHEN dof.gender = 'M' THEN 1 ELSE 0 END) as float8) / count(dof.gender)) as numeric), 2) as male_ratio
+FROM data_officer dof, data_officerhistory doh, data_policeunit dpu
+WHERE doh.officer_id = dof.id AND doh.unit_id = dpu.id AND dpu.description IS NOT NULL AND dpu.description != 'Unknown'
+GROUP BY dpu.description;
 
 -- What was Chicago’s income per capita? Which 3 - 5 neighborhoods have the highest and lowest income per capita?
 
