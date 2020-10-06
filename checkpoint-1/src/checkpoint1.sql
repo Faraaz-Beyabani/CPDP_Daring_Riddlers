b526a0b15f71765b8b79d1db1f0b24aa47559d34
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-10-06 11:31:27 -0500

    Clean up comments, add settlement query

--- checkpoint-1/src/checkpoint1.sql
+++ checkpoint-1/src/checkpoint1.sql
@@ -1,8 +1,4 @@
--- *** Which 5 counties have the highest crime rate, number of assigned police officers, complaints, ***
--- *** and settlements (for police misconduct) per 100,000 residents?                                ***
-
-
--- Officers by Unit
+-- What is the average crime rate, number of assigned officers, complaints, and settlements all of the Chicago area?
 
 -- Allegations by Beat
 SELECT
@@ -21,16 +17,21 @@ SELECT
 FROM data_allegation
 WHERE
     beat_id is not NULL AND
-    EXTRACT(year FROM incident_date) = 2015
+    EXTRACT(year FROM incident_date) = 2019
 GROUP BY beat_id
 ORDER BY COUNT(*) DESC
 
 
--- What is the average for these 4 statistics across all neighborhoods in Chicago / the Chicago area?
-
-
 
 -- Total Officers
+
+SELECT count(id) FROM data_officer
+WHERE resignation_date is null OR EXTRACT(year from resignation_date) >= 2000;
+
+SELECT count(id) FROM data_officer
+WHERE resignation_date is null OR EXTRACT(year from resignation_date) >= 2019;
+
+
 
 -- Total Allegations
 SELECT COUNT(*)
@@ -41,12 +42,36 @@ SELECT COUNT(*)
 FROM data_allegation
 WHERE EXTRACT(year FROM incident_date) = 2015
 
+
+
 -- Total Settlements
+
+-- 2011 (earliest available year for settlement data)
+
+SELECT sum(settlement)
+FROM lawsuit_payment
+WHERE EXTRACT(year from paid_date) = 2011;
+
+SELECT avg(settlement)
+FROM lawsuit_payment
+WHERE EXTRACT(year from paid_date) = 2011;
+
+-- 2019
+
+SELECT sum(settlement)
+FROM lawsuit_payment
+WHERE EXTRACT(year from paid_date) = 2019;
+
+SELECT avg(settlement)
+FROM lawsuit_payment
+WHERE EXTRACT(year from paid_date) = 2019;
+
+
 
 
 -- *** What is the racial and gender breakdown of the policing force (aggregating across all neighborhoods)? ***
 
--- 2000
+-- 2000 race and gender across the entire CPD
 
 SELECT race, count(race), gender, count(gender)
 FROM data_officer
@@ -54,7 +79,8 @@ WHERE EXTRACT(year FROM appointed_date) <= 2000
 AND EXTRACT(year FROM resignation_date) >= 2000 OR resignation_date is null
 GROUP BY race, gender
 
--- 2019
+
+-- 2019 race and gender across the entire CPD
 
 SELECT race, count(race), gender, count(gender)
 FROM data_officer
@@ -62,9 +88,11 @@ WHERE EXTRACT(year FROM appointed_date) <= 2019
 AND EXTRACT(year FROM resignation_date) >= 2019 OR resignation_date is null
 GROUP BY race, gender
 
+
+
 -- *** Which units have the highest male-to-female ratio of police officers? ***
 
--- 2000
+-- 2000 total gender distribution per unit
 
 SELECT dpu.description, dof.gender, count(dof.gender)
 FROM (SELECT * FROM data_officer WHERE EXTRACT(year FROM appointed_date) <= 2000
@@ -74,6 +102,9 @@ FROM (SELECT * FROM data_officer WHERE EXTRACT(year FROM appointed_date) <= 2000
 WHERE doh.officer_id = dof.id AND doh.unit_id = dpu.id AND dpu.description IS NOT NULL
 GROUP BY dpu.description, dof.gender;
 
+
+-- 2000 male-to-female ratio per unit
+
 SELECT dpu.description, round(cast((cast(sum(CASE WHEN dof.gender = 'M' THEN 1 ELSE 0 END) as float8) / count(dof.gender)) as numeric), 2) as male_ratio, count(dof.id) as num_employees
 FROM (SELECT * FROM data_officer WHERE EXTRACT(year FROM appointed_date) <= 2000
                                            AND (EXTRACT(year FROM resignation_date) >= 2000 OR resignation_date IS NULL))
@@ -82,7 +113,9 @@ FROM (SELECT * FROM data_officer WHERE EXTRACT(year FROM appointed_date) <= 2000
 WHERE doh.officer_id = dof.id AND doh.unit_id = dpu.id AND dpu.description IS NOT NULL AND dpu.description != 'Unknown'
 GROUP BY dpu.description;
 
--- 2019
+
+
+-- 2019 total gender distribution per unit
 
 SELECT dpu.description, dof.gender, count(dof.gender)
 FROM (SELECT * FROM data_officer WHERE EXTRACT(year FROM appointed_date) <= 2019
@@ -92,6 +125,9 @@ FROM (SELECT * FROM data_officer WHERE EXTRACT(year FROM appointed_date) <= 2019
 WHERE doh.officer_id = dof.id AND doh.unit_id = dpu.id AND dpu.description IS NOT NULL
 GROUP BY dpu.description, dof.gender;
 
+
+-- 2019 male-to-female ratio per unit
+
 SELECT dpu.description, round(cast((cast(sum(CASE WHEN dof.gender = 'M' THEN 1 ELSE 0 END) as float8) / count(dof.gender)) as numeric), 2) as male_ratio, count(dof.id) as num_employees
 FROM (SELECT * FROM data_officer WHERE EXTRACT(year FROM appointed_date) <= 2019
                                            AND (EXTRACT(year FROM resignation_date) >= 2019 OR resignation_date IS NULL))
@@ -100,11 +136,11 @@ FROM (SELECT * FROM data_officer WHERE EXTRACT(year FROM appointed_date) <= 2019
 WHERE doh.officer_id = dof.id AND doh.unit_id = dpu.id AND dpu.description IS NOT NULL AND dpu.description != 'Unknown'
 GROUP BY dpu.description;
 
--- *** What was Chicago’s income per capita? Which 3 - 5 neighborhoods have the highest and lowest income per capita? ***
+
 
 -- *** Which neighborhoods have predominantly White, Black, Asian, or Hispanic police forces? ***
 -- *** What is the race composition of every beat active in 2019?
-                                                
+
 SELECT res.beat, res.race, count(distinct res.officer_id) FROM
 (SELECT * FROM data_assignment_attendance
 WHERE EXTRACT(year FROM shift_start) = 2019 AND beat SIMILAR TO '[0-9]%') as res
@@ -112,7 +148,7 @@ WHERE res.officer_id is not null AND res.beat is not null
 GROUP BY res.beat, res.race
 
 -- *** What is the city’s racial breakdown for its population? ***
-                                                
+
 SELECT race, sum(count)
 FROM data_racepopulation drp
 GROUP BY race;
@@ -121,7 +157,9 @@ SELECT race, round(cast(avg(count) as numeric), 2)
 FROM data_racepopulation drp
 GROUP BY race;
 
--- *** Which neighborhoods are predominantly White, Black, Asian, Hispanic (compared to the city average)? ***
+
+
+-- *** Which areas are predominantly White, Black, Asian, Hispanic (compared to the city average)? ***
 
 SELECT drp.race, drp.area_id, da.name, drp.count, avg_race.average_count
 FROM (SELECT race, round(cast(avg(count) as numeric), 2) as average_count FROM data_racepopulation drp GROUP BY race) as avg_race,
